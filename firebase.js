8df973c416c77dd85d60543a82f1185110c6daeb
Codekacode <karem.quiroz.m@gmail.com>
2022-07-10 15:22:37 -0500

    Add header and inputo on sidebar

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDPjt-wF_-wDxaVYCFV0AF42M1-iyZNRb8",
+  authDomain: "whatsapp-clone-513e4.firebaseapp.com",
+  projectId: "whatsapp-clone-513e4",
+  storageBucket: "whatsapp-clone-513e4.appspot.com",
+  messagingSenderId: "332785640490",
+  appId: "1:332785640490:web:cc92bbf471155a0975cf53"
+}; 
