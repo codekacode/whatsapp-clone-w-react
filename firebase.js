42f1544625d4c41ac1be0c5c92bb03c1253521a5
Codekacode <karem.quiroz.m@gmail.com>
2022-07-10 22:17:08 -0500

    configure firebase auth, db and provider with google

--- firebase.js
+++ firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDPjt-wF_-wDxaVYCFV0AF42M1-iyZNRb8",
   authDomain: "whatsapp-clone-513e4.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
   messagingSenderId: "332785640490",
   appId: "1:332785640490:web:cc92bbf471155a0975cf53"
 }; 
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {auth,provider};
+export default db ;
--- src/Chat.js
+++ src/Chat.js
@@ -18,6 +18,7 @@ function Chat() {
   const sendMessage = (e) => {
     e.preventDefault();
     console.log(input)
+    setInput("");
   }
 
   return (
